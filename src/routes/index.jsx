59dca55258e653f1f1be9eecbbb521677633f46c
Sam Peck <fullstacksam@protonmail.com>
2023-08-27 21:25:47 -0400

    Implemented Newest React Router Features

---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+export default function Index(){
+  return(
+    <p id="zero-state">
+      Save your contacts!,
+      <br />
+      check out{" "}
+      <a href="mets.com">
+        The best sports team
+      </a>
+      .
+    </p>
+  );
+}
---
+++ src/src/error-page.jsx
@@ -0,0 +1,20 @@
+import { useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.log(error);
+
+
+return(
+  <div id="error-page">
+    <h1>Oops! We did it again!</h1>
+    <p>Sorry, an unexpected error has occurred.</p>
+    <p>
+      <i>{error.statusText || error.message}</i>
+    </p>
+
+  </div>
+);
+
+
+}
